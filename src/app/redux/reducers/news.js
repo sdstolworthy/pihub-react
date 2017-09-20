51d1b933bb8f84d79f0b772c7b12b37bc7643756
sdstolworthy <sdstolworthy@gmail.com>
2017-09-20 17:18:57 -0600

    started on news ui

--- src/app/redux/reducers/news.js
+++ src/app/redux/reducers/news.js
@@ -1,6 +1,6 @@
 import { SET_NEWS } from '../actions/news'
 
-export default function (state = {}, { type, ...action }) {
+export default function (state = [], { type, ...action }) {
   switch (type) {
     case SET_NEWS:
       return action.news;
